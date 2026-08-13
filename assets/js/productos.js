a9bf3fb17867d45b003366203cad073689fbf4ff
German Espinosa <germanespinosa1987@gmail.com>
2026-08-13 16:16:20 -0300

    Se confecciona escuchador de click en las cards

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,45 +1,51 @@
-fetch("../assets/data/productos.json")
-    .then((respuesta) => {
-        return respuesta.json();
-    })
-    .then((productos) => {
+async function cargarProductos() {
+
+    try {
+
+        const respuesta = await fetch("../assets/data/productos.json");
+
+        const productos = await respuesta.json();
 
         const contenedor = document.getElementById("contenedor-productos");
 
         productos.forEach((producto) => {
 
-            contenedor.innerHTML += `
-            <div class="card-producto">
-                <img src="${producto.imagen}" alt="${producto.nombre}">
-                <div class="card-content">
-                    <h3>${producto.nombre}</h3>
-                    <p>${producto.descripcion}</p>
-                    <p><strong>$ ${producto.precio.toLocaleString("es-AR")}</strong></p>
+        contenedor.innerHTML += `
+        <div class="card-producto">
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <div class="card-content">
+                <h3>${producto.nombre}</h3>
+                <p>${producto.descripcion}</p>
+                <p><strong>$ ${producto.precio.toLocaleString("es-AR")}</strong></p>
 
-                    <button
-                        class="btn btn-agregar"
-                        data-id="${producto.id}">
-                        Agregar al presupuesto
-                    </button>
-                </div>
+                <button
+                    class="btn btn-agregar"
+                    data-id="${producto.id}">
+                    Agregar al presupuesto
+                </button>
             </div>
+        </div>
         `;
 
-        });
-        const botonesAgregar = document.querySelectorAll(".btn-agregar");
+    const botonesAgregar = document.querySelectorAll(".btn-agregar");
 
-        botonesAgregar.forEach((boton) => {
+botonesAgregar.forEach((boton) => {
 
-            boton.addEventListener("click", () => {
-
-                agregarAlPresupuesto(Number(boton.dataset.id));
-
-            });
-
-        });
-
-    })
-
-    .catch((error) => {
-        console.error(error);
+    boton.addEventListener("click", () => {
+        const idProducto = Number(boton.dataset.id);
+        agregarAlPresupuesto(idProducto);
     });
+
+});
+
+}); 
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+cargarProductos();
+
+const productoSeleccionado = productos.find((producto) => producto.id === idProducto);
